b5f7106d39a3e721632ec19831ba6fe11b814ff2
Alexa Botello <abotello20@yahoo.com>
2020-10-16 04:37:29 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,19 @@
+def using_push(colors_in_the_rainbow, violet)
+  colors_in_the_rainbow.push("violet")
+end 
+
+def using_unshift(bouroughs_in_nyc, staten_island)
+    bouroughs_in_nyc.unshift("Staten Island")
+end
+
+def using_pop(continents)
+  anartica = continents.pop
+end
+
+def pop_with_args(dog_breeds)
+  small_dogs = dog_breeds.pop (2)
+end
+
+def using_shift(my_favorite_cities)
+  im_so_over_this_city = using_shift(my_favorite_cities)
+end 
